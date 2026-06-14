7e9455dbd772721714cba34a3bf5a813281ea5ad
Ashirwad kumar <ashirwad@Ashirwads-MacBook-Air.local>
2026-06-14 09:34:30 +0530

    feat: complete explore and education sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
+import { Routes, Route } from "react-router-dom";
 
-import About from './About/About'
-import './App.css'
-import Home from './Home/home'
-import Skill from './Skills/Skill'
+
+import Projects from "./Explore/Projects/Projects";
+import Home from "./Home/home";
+import About from "./About/About";
+import Skill from "./Skills/Skill";
+import Explore from "./Explore/Explore";
+import Photography from "./Explore/Photography/Photography";
+import Books from "./Explore/Books/Books";
+import Blogs from "./Explore/Blogs/Blogs";
+import Education from "./Education/Education";
+
+
+
 
 
 function App() {
-  
-
   return (
-   <div>
-    {/* <h1>App</h1> */}
-    <Home />
-    <About />
-    <Skill />
-   </div>
-  )
+    <Routes>
+
+      <Route
+        path="/"
+        element={
+          <>
+            <Home />
+            <About />
+            <Skill />
+            <Explore />
+            <Education />
+          </>
+        }
+      />
+
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/photography" element={<Photography />} />
+      <Route path="/books" element={<Books />} />
+      <Route path="/blogs" element={<Blogs />} />
+
+    </Routes>
+  );
 }
 
-export default App
+export default App;
